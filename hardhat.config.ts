c8ace7d18b76235b60a647bafd72f73a1c5e11c9
rahman Orewole <rorewole@gmail.com>
2025-02-16 15:34:02 +0100

    deploy to sepolia config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY as string],
+    },
+  },
 };
 
 export default config;
